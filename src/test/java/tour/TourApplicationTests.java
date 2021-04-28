99794ac80596b6391a3e442242fe3dddda5d76ae
hongbb080 <hongbb080@naver.com>
2021-04-29 00:08:03 +0900

    기본 파일

---
+++ src/test/java/tour/TourApplicationTests.java
@@ -0,0 +1,13 @@
+package tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
