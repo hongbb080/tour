5b089ae2de8ca7420ab123feb9f66bdd9b47fa53
hongbb080 <hongbb080@naver.com>
2021-04-29 00:06:55 +0900

    후기 dto

---
+++ src/main/java/tour/dto/PickDto.java
@@ -0,0 +1,13 @@
+package tour.dto;
+
+import lombok.Data;
+
+@Data
+public class PickDto {
+	private int num;
+	private String writer;
+	private String title;
+	private String comment;
+	private String createdDay;
+	private String nickname;
+}	
---
+++ src/main/java/tour/dto/PickDto2.java
@@ -0,0 +1,16 @@
+package tour.dto;
+
+import lombok.Data;
+
+@Data
+public class PickDto2 {
+		private int reviewnum;
+		private String userid;
+		private String comments;
+		private String createdtime;
+		private String uc;
+		private String nickname;
+}
+
+	
+
